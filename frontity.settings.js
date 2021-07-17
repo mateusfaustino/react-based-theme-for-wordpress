fb56618398f423180d3b0e17752f47985089c192
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-07-17 10:15:50 -0300

    Change Theme

--- frontity.settings.js
+++ frontity.settings.js
@@ -8,9 +8,25 @@ const settings = {
     }
   },
   "packages": [
-    {
-      "name":"mateus-theme"
-    },
+    
+{
+  name: "@frontity/mars-theme",
+  state: {
+    theme: {
+      menu: [
+        ["Home", "/"],
+        ["Nature", "/category/nature/"],
+        ["Travel", "/category/travel/"],
+        ["Japan", "/tag/japan/"],
+        ["About Us", "/about-us/"]
+      ],
+      featured: {
+        showOnList: false,
+        showOnPost: false
+      }
+    }
+  }
+},
     {
       "name": "@frontity/wp-source",
       "state": {
